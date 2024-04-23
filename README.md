d6a73d8b6f714b3bcd9966e3a5b81fc0ca07ce47
Eric Xue <eric.r.xue@gmail.com>
2024-04-22 20:45:07 -0400

    arg parser

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cos518-2024-project
 
-run `cargo run -- replica` to start a server and then run `cargo run -- client`
+1) run `cargo run -- -r 127.0.0.1:6000/127.0.0.1:6001` to start a replica with address 127.0.0.1:6000 and API address 127.0.0.1:6001 
+2) run `cargo run -- -c 127.0.0.1:6000` to start the client
+
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,55 @@ pub mod network;
 pub mod types;
 
 use std::env;
+use std::str::FromStr;
 use network::client;
 use network::replica;
 use network::replica::Replica;
 use smol::io;
+use clap::Parser;
+use std::net::SocketAddrV4;
+
+
+/// 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// List of replica and API address pairs (format: e.g., 127.0.0.1:6000/127.0.0.1:6001)
+    #[arg(short, long, value_delimiter = ' ', num_args = 1..)]
+    replicas: Vec<String>,
+
+    /// List of client addresses (format: e.g., 127.0.0.1:6000)
+    #[arg(short, long, value_delimiter = ' ', num_args = 1..)]
+    clients: Vec<String>,
+}
+
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    if args[1] == "replica" {
-        let mut replica = Replica::new(1, ([127, 0, 0, 1], 6000), ([127, 0, 0, 1], 6001));
+    for addrs in cli.replicas.iter() {
+        // debugging
+        println!("replica input: {}", addrs);
+
+        let addrs = addrs.split("/").collect::<Vec<&str>>();
+
+        // debugging
+        println!("replica address: {}", addrs[0]);
+        println!("api address: {}", addrs[1]);
+        
+        let replica_socket = SocketAddrV4::from_str(addrs[0]).unwrap();
+        let api_socket = SocketAddrV4::from_str(addrs[1]).unwrap();
+        let mut replica = Replica::new(1, (replica_socket.ip().octets(), replica_socket.port()), (api_socket.ip().octets(), api_socket.port()));
         let _res = replica.start();
-    } else if args[1] == "client" {
-        let _res = client::run_client(([127, 0, 0, 1], 6000));
-    } else {
-        println!("not recognized: {}", args[1])
     }
-    
+
+    for addr in cli.clients.iter() {
+        // debugging
+        println!("client address: {}", addr);
+        
+        let socket = SocketAddrV4::from_str(addr).unwrap();
+        let _res = client::run_client((socket.ip().octets(), socket.port()));
+    }
 
     Ok(())
 }
