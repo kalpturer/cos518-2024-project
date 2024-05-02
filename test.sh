9bdd70ec7f8b53dfdd0fb1410dc5e86a1a8004ae
Eric Xue <eric.r.xue@gmail.com>
2024-05-02 10:07:25 -0400

    changed ports

--- test.sh
+++ test.sh
@@ -7,21 +7,21 @@ cargo build
 echo "Starting 3 replicas"
 
 # Start replica 1
-./target/debug/project --id 1 -n 3 --listener 127.0.0.1:6000 --connections 127.0.0.1:7000 127.0.0.1:8000 > replica1.log 2> replica1.err &
+./target/debug/project --id 1 -n 3 --listener 127.0.0.1:6000 --connections 127.0.0.1:8000 127.0.0.1:9000 > replica1.log 2> replica1.err &
 # Get its process ID
 PID1=$!
 # Sleep for 1 second
 sleep 1
 
 # Start replica 2
-./target/debug/project --id 2 -n 3 --listener 127.0.0.1:7000 --connections 127.0.0.1:6000 127.0.0.1:8000 > replica2.log 2> replica2.err &
+./target/debug/project --id 2 -n 3 --listener 127.0.0.1:8000 --connections 127.0.0.1:6000 127.0.0.1:9000 > replica2.log 2> replica2.err &
 # Get its process ID
 PID2=$!
 # Sleep for 1 second
 sleep 1
 
 # Start replica 3
-./target/debug/project --id 3 -n 3 --listener 127.0.0.1:8000 --connections 127.0.0.1:6000 127.0.0.1:7000 > replica3.log 2> replica3.err &
+./target/debug/project --id 3 -n 3 --listener 127.0.0.1:9000 --connections 127.0.0.1:6000 127.0.0.1:8000 > replica3.log 2> replica3.err &
 # Get its process ID
 PID3=$!
 # Sleep for 5 seconds
@@ -36,11 +36,11 @@ echo "Starting 3 client request generators"
 PID4=$!
 sleep 1
 
-./target/debug/project --gen 127.0.0.1:7000 > /dev/null 2>&1 &
+./target/debug/project --gen 127.0.0.1:8000 > /dev/null 2>&1 &
 PID5=$!
 sleep 1
 
-./target/debug/project --gen 127.0.0.1:8000 > /dev/null 2>&1 &
+./target/debug/project --gen 127.0.0.1:9000 > /dev/null 2>&1 &
 PID6=$!
 sleep 1
 echo "Finished starting 3 client request generators"
@@ -55,9 +55,9 @@ echo "Saving replica states"
 # Fetch the longest chain transactions from all three nodes and write the output to files
 ./target/debug/project --save 127.0.0.1:6000 > /dev/null 2>&1 &
 PID7=$!
-./target/debug/project --save 127.0.0.1:7000 > /dev/null 2>&1 &
-PID8=$!
 ./target/debug/project --save 127.0.0.1:8000 > /dev/null 2>&1 &
+PID8=$!
+./target/debug/project --save 127.0.0.1:9000 > /dev/null 2>&1 &
 PID9=$!
 echo "Finished saving replica states"
 
